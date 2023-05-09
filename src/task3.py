34cecf6a227aec52316c7ec3ef919aa84441166e
TUoS Student <student@TENB885849C727E.shefuniad.shef.ac.uk>
2023-05-09 19:16:12 +0100

    task3 added

--- src/task3.py
+++ src/task3.py
@@ -28,33 +28,97 @@ class Task3:
         self.ctrl_c = True
 
     def scan_callback(self,scan_data):
-        front_left_view = scan_data.ranges[0:71]
-        front_right_view = scan_data.ranges[-70:]
-        left_view = scan_data.ranges[40:70]
+
+        # NOT SURE IF NEEDED ---------
+        #direct_left_view = scan_data.ranges[65:115]
+        #direct_left_array = np.array(direct_left_view)
+        #self.minD_direct_left = direct_left_array.min()
+
+        #back_view = scan_data.ranges[160:200]
+        #back_arc = np.array(back_view)
+        #self.minD_back = back_arc.min()
+
+        
+                #   Front Dist < 0.3 = Stop Moving
+
+                #if (self.minD_left < 0.25):
+                    #self.vel.angular.z = -1.8 + (self.minA_left * 0.04)
+
+                    #if (self.minD_left < 0.15 or self.minD_front < 0.15):
+                        #self.vel.linear.x = 0.00
+                    #else : 
+                        #self.vel.linear.x = (self.minD_left * 0.8)
+                #else:
+                    #if self.minD_front < 0.6:
+                        #self.vel.angular.z = -0.5 - ((0.6-self.minD_front)* (10/3) )
+                        #self.vel.linear.x = 0.2 - ((0.6 - self.minD_front)*0.4)
+                    #else: 
+                        #self.vel.angular.z = -1.4
+                    #20% corner, goes straight into wall as it doesn't turn as fast
+                    #calculate angle by how close wall in front is
+                    # 0.3 - 0.6, 0.3 = -1.5 , 0.6 = -0.5
+                    # closer = faster turn,
+                    #self.vel.linear.x = 0.20
+                    #if (self.minD_right > 0.44 and self.minD_right < 0.6):
+                        #self.vel.angular.z = -0.5
+                    #else :
+                        #self.vel.angular.z = -1.5
+                    #if self.minD_left > 0.25:
+                        #self.vel.angular.z = -1.2
+                    #elif self.minD_right < 0.6:
+                        #self.vel.angular.z = self.minD_right * -3
+                   # else:
+                        #self.vel.angular.z = -1.2
+        # -------
+
+        # Front left side view
+        left_view = scan_data.ranges[40:80]
         left_array = np.array(left_view)
+        # Gets distance (m) that is the closest
+        self.minD_left = left_array.min()
+        # Gets angle (degree) attached to the closest distance
+        # LEFT SIDE: 0 = In front of bot , 40 = side of bot
+        self.minA_left = left_array.argmin()
+
         right_view = scan_data.ranges[280:320]
         right_array = np.array(right_view)
+        # Gets distance (m) that is the closest on the right
+        self.minD_right = right_array.min()
+        # Gets angle (degree) that is the attached to the closest distance
+        # RIGHT SIDE: 40 = In front of bot , 0 = side of bot
+        self.minA_right = right_array.argmin()
+        
+        # View of directly in front of bot 
         left_arc = scan_data.ranges[0:21]
         right_arc = scan_data.ranges[-20:]
-        full_front_arc = np.array(front_left_view[::-1] + front_right_view[::-1])
         front_arc = np.array(left_arc[::-1] + right_arc[::-1])
-        maxD_front = full_front_arc.max()
-        minD_front = front_arc.min()
-        self.minD_left = left_array.min()
-        self.minD_right = right_array.min()
-        if (minD_front < 0.25):
-            self.turn = "noCrash"
-        elif ((self.minD_left > 0.45 or self.minD_right < 0.25) and minD_front > 0.30):
+        self.minD_front = front_arc.min()
+        maxD_front = front_arc.max()
+
+        # MAYBE ADD as failsafe, get rid if it affects too much normal moving
+        #if (self.minD_front < 0.25 or self.minD_full_front < 0.15):
+            #self.turn = "noCrash"
+
+        # Distance between left and right wall
+        # +ve = Closer to right wall , -ve = Closer to left
+        self.left_right_dist = self.minD_left - self.minD_right
+        if (self.minD_left < 0.5 and self.minD_right < 0.5 and maxD_front < 0.55):
+            self.turn = "uTurn"
+        elif (self.left_right_dist > 0.1):
             self.turn = "Left"
-        elif ((self.minD_left < 0.25 or self.minD_right > 0.5) and minD_front > 0.30):
+        elif (self.minD_front < 0.3):
+            self.turn = "stopLeft"
+        elif (self.left_right_dist < -0.1):
             self.turn = "Right"
+        elif (self.minD_left > 0.45):
+            self.turn = "Left"
         elif (maxD_front < 0.65):
             self.turn = "uTurn"
-        elif (minD_front < 0.6 and self.minD_right > 0.4):
-            self.turn = "Right"
         else:
             self.turn = "No"
-        self.minInfo = f"Front: '{minD_front:.2f}',  Left: '{self.minD_left:.2f}', Right: '{self.minD_right:.2f}', MaxFront: '{maxD_front:.2f}'"
+
+        #Prints a lot of data
+        self.minInfo = f"Front: '{self.minD_front:.2f}',  Left: '{self.minD_left:.2f}', Right: '{self.minD_right:.2f}', MaxFront: '{maxD_front:.2f}', leftMinA: '{self.minA_left:.2f}',rightMinA: '{self.minA_right:.2f}'"
 
     def odometry_callback(self,odom):
         pose = odom.pose.pose
@@ -68,7 +132,7 @@ class Task3:
                      curr_y, curr_z, orientation.w], 
                      'sxyz')
         #idk if we need you g x
-        if self.counter > 15:
+        if self.counter > 25:
             self.counter = 0
             print(f"'{self.minInfo}'")
             print(f"he is turning'{self.turn}'")
@@ -76,37 +140,86 @@ class Task3:
             self.counter += 1
 
     def main_loop(self):
-        rate = rospy.Rate(30)
+        rate = rospy.Rate(40)
         while not self.ctrl_c:
             if self.turn == "Left":
-                self.vel.linear.x = 0.20
-                if self.minD_right > 0.25:
-                    self.vel.angular.z = 1.5
-                elif self.minD_left < 0.9:
-                    self.vel.angular.z = self.minD_left * 2
+                # IF TOO CLOSE TO THE RIGHT WALL
+                if (self.minD_right < 0.25):
+                    #Turning Speed 
+                    # LB 0.2 : Closest angle is directly too right
+                    # UB 1.8 : Wall close to in front 
+                    self.vel.angular.z = 0.1 + (self.minA_right * 0.04)
+
+                    #Linear Speed
+                    # If very close to right, don't move
+                    if (self.minD_right < 0.15):
+                        self.vel.linear.x = 0.00
+                    
+                    # Otherwise, scale (0.15m to 0.25m)
+                    else : 
+                        # 0.25m = 0.2m/s
+                        # 0.15m = 0.12m/s
+                        #self.vel.linear.x = (self.minD_right * 0.8)
+                        # 0.25m = 0.15m/s
+                        # 0.15m = 0.05m/s
+                        self.vel.linear.x = 0.05 +((0.25 - self.minD_right))
+                    
+                # IF TURNING BECAUSE TOO FAR FROM LEFT WALL
                 else:
-                    self.vel.angular.z = 1
-            elif self.turn == "Right":
-                self.vel.linear.x = 0.20
-                if self.minD_left > 0.25:
-                    self.vel.angular.z = -1.5
-                elif self.minD_right < 0.9:
-                    self.vel.angular.z = self.minD_right * -2
-                else:
-                    self.vel.angular.z = 1
+
+                    #If closer than 0.6m away from wall in front, scale turning and linear speed
+                    if self.minD_front < 0.6:
+                        self.vel.angular.z = 0.35 + ((0.6-self.minD_front)* (10/3) )
+                        self.vel.linear.x = 0.25 - ((0.6 - self.minD_front)*0.4)
+                        #0.1 = - 0.2
+                        #0.6 = - 0 
+
+                    #Further than 0.6m away from front wall
+                    else:
+                        self.vel.linear.x = 0.20
+                        self.vel.angular.z = 1.4
+
+
+            elif self.turn == "stopLeft":
+                #When linear speed is -ve, angular needs to be the negation of whichever way
+                #you actually wanna go
+                self.vel.angular.z = -1
+                self.vel.linear.x = -0.2
+
+            elif self.turn == "Right" or self.turn =="Right2":
+                #HOW FAST TURN based on CLOSEST ANGLE
+                #   Angle = closer to front = turn faster (1.8)
+                #         = closer to side  = turn slower (0.3)
+                 
+                #HOW FAST MOVE based on CLOSEST METER
+                #   Distance = lower (<0.25) = move slower (0.05)
+                #            = higher (> 0.7) = move faster (0.25)
+                # if self.minA_left <
+                
+                self.vel.angular.z = -1.7 + (self.minA_left * 0.04)
+                if self.minD_front < 0.6:
+                    self.vel.linear.x = 0.18 - ((0.6 - self.minD_front)*0.4)
+                else: 
+                    self.vel.linear.x = 0.25
+            
             elif self.turn == "noCrash":
-                self.vel.linear.x = -0.15
-                self.vel.angular.z = 0
-                self.pub.publish(self.vel)
+                front_back_range = self.minD_front - self.minD_back
+                while (front_back_range > 0.05 or front_back_range < -0.05):
+                    front_back_range = self.minD_front - self.minD_back
+                    if front_back_range > 0:
+                        self.vel.linear.x = 0.1
+                    else : 
+                        self.vel.linear.x = -0.1
+                    self.vel.angular.z = 0
+                    self.pub.publish(self.vel)          
+                #move him between the area in front and behind him
+                #while loop until the middle then continue
                 rospy.sleep(1)
-                self.pub.publish(Twist())
-                rospy.sleep(1)
+
             elif self.turn == "uTurn":
-                self.vel.linear.x = 0
+                self.vel.linear.x = 0.018
                 self.vel.angular.z = math.pi/2
                 self.pub.publish(self.vel)
-                rospy.sleep(2)
-                self.pub.publish(Twist())
                 rospy.sleep(2)
             else:
                 self.vel.linear.x = 0.25
